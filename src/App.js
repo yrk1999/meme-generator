e81ee2a00a7294a643bb6ed402070a2cb0104a2b
yrk1999 <khasnisyash42@gmail.com>
2022-03-14 11:42:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from "./components/Navbar"
+import Meme from "./components/Meme"
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="/images/troll-face.png" alt="TrollFace logo" className="nav--logo" />
+            <span className="nav--title">Meme Generator</span>
+            <span className="nav--project">React Course - Project 3</span>
+        </nav>
+    )
+}
